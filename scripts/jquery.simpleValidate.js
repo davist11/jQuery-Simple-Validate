097e38244db1e9e40a4565ce6b5b5ad0b0bd6c49
Trevor Davis <davist11@gmail.com>
2012-08-04 17:28:31 -0400

    complete rewrite to use new plugin pattern

--- scripts/jquery.simpleValidate.js
+++ scripts/jquery.simpleValidate.js
@@ -7,82 +7,120 @@
  * Uses the same license as jQuery, see:
  * http://jquery.org/license
  *
- * @version 0.2
+ * @version 0.3
  *
  * Example usage:
  * $('form.required-form').simpleValidate({
- *   errorClass: 'error',
- *   errorText: '{label} is a required field.',
- *   emailErrorText: 'Please enter a valid {label}',
- *   errorElement: 'strong',
- *   removeLabelChar: '*',
- *   inputErrorClass: '',
- *   completeCallback: '',
- *   ajaxRequest: false
+ *	 errorClass: 'error',
+ *	 errorText: '{label} is a required field.',
+ *	 emailErrorText: 'Please enter a valid {label}',
+ *	 errorElement: 'strong',
+ *	 removeLabelChar: '*',
+ *	 inputErrorClass: '',
+ *	 completeCallback: '',
+ *	 ajaxRequest: false
  * });
  */
-;(function($) {
-	$.fn.simpleValidate = function(options) {
-		var opts = $.extend({}, $.fn.simpleValidate.defaults, options);
+;(function($, window, document, undefined){
 
-		return this.each(function() {
-			var $this = $(this),
-					o = $.meta ? $.extend({}, opts, $this.data()) : opts,
-					errorMsgType = o.errorText.search(/{label}/);
+	// our plugin constructor
+	var SimpleValidate = function(elem, options) {
+		this.elem = elem;
+		this.$elem = $(elem);
+		this.options = options;
+		this.metadata = this.$elem.data('plugin-options');
+		this.$requiredInputs = this.$elem.find(':input.required');
+	};
+
+	// the plugin prototype
+	SimpleValidate.prototype = {
+		defaults: {
+			errorClass: 'error',
+			errorText: '{label} is a required field.',
+			emailErrorText: 'Please enter a valid {label}',
+			errorElement: 'strong',
+			removeLabelChar: '*',
+			inputErrorClass: '',
+			completeCallback: '',
+			ajaxRequest: false
+		},
+
+		init: function() {
+			var self = this;
+
+			// Introduce defaults that can be extended either
+			// globally or using an object literal.
+			self.config = $.extend({}, self.defaults, self.options, self.metadata);
 			
-			//When the form is submitted
-			$this.bind('submit', function(e) {
-				var hasError = false;
-				
-				//Hide any errors that are already showing
-				$this.find(o.errorElement + '.' + o.errorClass).remove();
-				$this.find(':input.' + o.inputErrorClass).removeClass(o.inputErrorClass);
-				
-				//Get all the required inputs
-				$this.find(':input.required').each(function() {
-					var $input = $(this),
-							fieldValue = $.trim($input.val()),
-							labelText = $input.siblings('label').text().replace(o.removeLabelChar, ''),
-							errorMsg = '';
-					
-					//Check if it's empty or an invalid email
-					if(fieldValue === '') {
-					  errorMsg = (errorMsgType > -1 ) ? errorMsg = o.errorText.replace('{label}',labelText) : errorMsg = o.errorText;
-						hasError = true;
-					} else if($input.hasClass('email')) {
-					  if(!(/^([_a-z0-9-]+)(\.[_a-z0-9-]+)*@([a-z0-9-]+)(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/.test(fieldValue.toLowerCase()))) {
-					    errorMsg = (errorMsgType > -1 ) ? errorMsg = o.emailErrorText.replace('{label}',labelText) : errorMsg = o.emailErrorText;
-					    hasError = true;
-					  }
-					}
-					
-					//If there is an error, display it
-					if(errorMsg !== '') {
-					  $input.addClass(o.inputErrorClass).after('<'+o.errorElement+' class="'+o.errorClass+'">' + errorMsg + '</'+o.errorElement+'>');
-					}
-				});
-				
-				if(hasError) { //Don't submit the form if there are errors
-					e.preventDefault();
-				} else if(o.completeCallback !== '') { //If there is a callback
-					o.completeCallback($this);
-					if(o.ajaxRequest) { //If AJAX request
-						e.preventDefault();
-					}
+			// What type of error message is it
+			self.errorMsgType = self.config.errorText.search(/{label}/);
+			
+			self.$elem.on('submit.simpleValidate', $.proxy(self.handleSubmit, self));
+
+			return this;
+		},
+		
+		checkField: function(index) {
+			var self = this;
+			var $field = self.$requiredInputs.eq(index);
+			var fieldValue = $.trim($field.val());
+			var labelText = $field.siblings('label').text().replace(self.config.removeLabelChar, '');
+			var errorMsg = '';
+			
+			//Check if it's empty or an invalid email and format the error message
+			if(fieldValue === '') {
+				errorMsg = self.formatErrorMsg(self.config.errorText, labelText);
+				self.hasError = true;
+			} else if($field.hasClass('email')) {
+				if(!(/^([_a-z0-9-]+)(\.[_a-z0-9-]+)*@([a-z0-9-]+)(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/.test(fieldValue.toLowerCase()))) {
+					errorMsg = self.formatErrorMsg(self.config.emailErrorText, labelText);
+					self.hasError = true;
 				}
-			});
+			}
+			
+			//If there is an error, display it
+			if(errorMsg !== '') {
+			  $field.addClass(self.config.inputErrorClass).after('<' + self.config.errorElement + ' class="' + self.config.errorClass + '">' + errorMsg + '</' + self.config.errorElement + '>');
+			}
+		},
+		
+		formatErrorMsg: function(errorText, labelText) {
+			return (this.errorMsgType > -1 ) ? errorText.replace('{label}', labelText) : errorText;
+		},
+		
+		handleSubmit: function(e) {
+			var self = this;
+			
+			// We are just starting, so there are no errors yet
+			this.hasError = false;
+			
+			// Reset existing displayed errors
+			self.$elem.find(self.config.errorElement + '.' + self.config.errorClass).remove();
+			self.$elem.find(':input.' + self.config.inputErrorClass).removeClass(self.config.inputErrorClass);
+			
+			// Check each field
+			self.$requiredInputs.each($.proxy(self.checkField, self));
+			
+			// Don't submit the form if there are errors
+			if(self.hasError) { 
+				e.preventDefault();
+			} else if(self.config.completeCallback !== '') { // If there is a callback
+				self.config.completeCallback(this.$elem);
+				
+				// If AJAX request
+				if(self.config.ajaxRequest) {
+					e.preventDefault();
+				}
+			}
+		}
+	};
+
+	SimpleValidate.defaults = SimpleValidate.prototype.defaults;
+
+	$.fn.simpleValidate = function(options) {
+		return this.each(function() {
+			new SimpleValidate(this, options).init();
 		});
 	};
 
-	// default options
-	$.fn.simpleValidate.defaults = {
-		errorClass: 'error',
-		errorText: '{label} is a required field.',
-		emailErrorText: 'Please enter a valid {label}',
-		errorElement: 'strong',
-		removeLabelChar: '*',
-		inputErrorClass: '',
-		completeCallback: '',
-		ajaxRequest: false
-	};
-})(jQuery);
+})( jQuery, window , document );
